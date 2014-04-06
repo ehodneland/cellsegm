05f8e9790bdbce63a1af30f8ad3a31d195efd55a
Erlend Hodneland <erlend.hodneland@gmail.com>
2014-04-06 13:47:36 +0200

    Adding two visualization tools: showall.m (3D data) and show4D.m (4D data)

--- @cellsegm/showall.m
+++ @cellsegm/showall.m
@@ -1,3 +1,4 @@
+function [] = showall(varargin)
 % SHOWALL Show one plane awt a time for stack.
 %
 %   SHOWALL(IM) Shows all planes in a 3D stack succesively when hitting ENTER. 
@@ -7,8 +8,26 @@
 %   planes. If the last argument is 'colorbar', then the images are plotted 
 %   with the colorbar option. 
 %
+%
+%     =======================================================================================
+%     Copyright (C) 2013  Erlend Hodneland
+%     Email: erlend.hodneland@biomed.uib.no 
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%     =======================================================================================
+%
 
-function [] = showall(varargin)
 
 numim = 0;
 col = 0;
